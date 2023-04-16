c88ede1e20c35d2307fbe884f991ba19f6aafaf1
Jules Carpentier <jules.o.carpentier@protonmail.com>
2023-04-16 21:33:22 +0200

    append works
    corrected use of append with numbers 2 and 4 --> 1 and 3

--- rocket accounting.py
+++ rocket accounting.py
@@ -1,16 +1,6 @@
 # import module
 from tabulate import tabulate
 
-# def tab(actif,Sa,passif,Sp):
-# # assign data
-#     mydata = [
-#         [actif,Sa,passif,Sp],
-#         ]
-# # create header
-#     head = ["Actifs","somme", "Passifs","somme"]
-# # display table
-#     print(tabulate(mydata, headers=head, tablefmt="grid"))
-    
 actif=[]
 passif=[]
 data = []
@@ -19,16 +9,8 @@ def tab(n):
     data.append(n)
     head = ["Actifs","somme", "Passifs","somme"]
     print(tabulate(data, headers=head, tablefmt="grid"))
-    # L=[n]
-    print(type(n))
-    # actif.append(n[2])
-    # passif.append(n[4])
-
-# def act(actif,Sa,passif,Sp):
-#     L=[actif,Sa,passif,Sp]
-#     print(L)
-    
-
+    actif.append(n[1])
+    passif.append(n[3])
 
 # ATTENTION: écrire dans console sous forme : 
     # tab(["mobilier",2000,"fournisseurs",2000])
