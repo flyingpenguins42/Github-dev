bbbea89822d5a8a692744107cc9aeff07a1da2bf
Jules Carpentier <jules.o.carpentier@protonmail.com>
2023-04-16 22:10:01 +0200

    added little print functions

--- rocket accounting.py
+++ rocket accounting.py
@@ -14,10 +14,16 @@ def tab(n):
     actif.append(n[1]) 
     passif.append(n[3]) #on ajoute les valeurs A et P à leurs listes
     SumA= sum(actif)
-    SumP= sum(passif)
+    SumP= sum(passif) #2 variables égales aux sommes des A et P
     print ("ACTIFS=", SumA)
-    print ("PASSIFS=", SumP)
-
+    print ("PASSIFS=", SumP) #affichage des valeurs des A et P
+    
+    if SumA > SumP :
+        print("il doit y avoir erreur dans vos calculs")
+    elif SumA < SumP:
+        print("oups,vous êtes en faillite..")
+    elif SumA == SumP:
+        print("les valeurs sont justes")
     
 # ATTENTION: écrire dans console sous forme : 
     # tab(["mobilier",2000,"fournisseurs",2000])
