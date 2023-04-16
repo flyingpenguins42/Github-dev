2e42334b4b6dd13f38a606a600072f0368354120
Jules Carpentier <jules.o.carpentier@protonmail.com>
2023-04-16 22:02:29 +0200

    added actives and passives values
    created 2 variables and print them with function tab

--- rocket accounting.py
+++ rocket accounting.py
@@ -1,16 +1,24 @@
 # import module
 from tabulate import tabulate
 
+data = [] #la liste qui va contenir les données du tableau
+
 actif=[]
-passif=[]
-data = []
+passif=[] #les listes annexes pour sommer indépendement les actifs et les passifs
 
 def tab(n):
-    data.append(n)
-    head = ["Actifs","somme", "Passifs","somme"]
-    print(tabulate(data, headers=head, tablefmt="grid"))
-    actif.append(n[1])
-    passif.append(n[3])
+    data.append(n) #on ajoute les éléments à la liste data
+    head = ["Actifs","somme", "Passifs","somme"] #le titre
+    print(tabulate(data, headers=head, tablefmt="grid")) #afficher le tableau
+    
+    actif.append(n[1]) 
+    passif.append(n[3]) #on ajoute les valeurs A et P à leurs listes
+    SumA= sum(actif)
+    SumP= sum(passif)
+    print ("ACTIFS=", SumA)
+    print ("PASSIFS=", SumP)
 
+    
 # ATTENTION: écrire dans console sous forme : 
     # tab(["mobilier",2000,"fournisseurs",2000])
+    
