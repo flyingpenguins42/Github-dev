69ab1a8d6fd5fa67ca790687760561f6d7ea3ef7
Jules Carpentier <jules.o.carpentier@protonmail.com>
2023-04-17 11:50:14 +0200

    added "order" list and n variable
    replaced n factor from tab function by e
    --> problem with n variable

--- rocket accounting.py
+++ rocket accounting.py
@@ -1,6 +1,7 @@
 from tabulate import tabulate
 
 data = [] #la liste qui va contenir les données du tableau
+order = [] #la liste qui va numéroter l'ordre d'affichage des lignes
 
 actif=[]
 passif=[] #les listes annexes pour sommer indépendement les actifs et les passifs
@@ -8,25 +9,30 @@ passif=[] #les listes annexes pour sommer indépendement les actifs et les passi
 # ATTENTION: écrire dans console sous forme : 
     # tab(["mobilier",2000,"fournisseurs",2000])
     # ou tab([1010,2000,2000,2000])
-
-def tab(n):
+n=0
+def tab(e):
+    data.append(e) #on ajoute les éléments à la liste data
     
-    data.append(n) #on ajoute les éléments à la liste data
-    if n[0] == 1000:
-        n[0]="caisse"
-    if n[0] == 10:
-        n[0]="caisse"
-        #compléter avec autres noms
+    if e[0] == 1000:
+        e[0]="caisse"
+        # order.append()
+        #compléter avec autres noms Actifs
 
-    if n[3] ==2000:
-        n[3]="fournisseurs"
-        #compléter avec autres noms
+    if e[2] ==2000:
+        e[2]="fournisseurs"
+        #compléter avec autres noms Passifs
     
-    head = ["Actifs","somme", "Passifs","somme"] #le titre
+    #ajouter fonction pour réaficher tableau dans le bon ordre à chaque fois
+    print ("order=",order)
+    n+=1
+    print(n)
+    
+    head = ["Actifs","Valeur", "Passifs","Valeur"] #le titre
     print(tabulate(data, headers=head, tablefmt="grid")) #afficher le tableau
     
-    actif.append(n[1]) 
-    passif.append(n[3]) #on ajoute les valeurs A et P à leurs listes
+    
+    actif.append(e[1]) 
+    passif.append(e[3]) #on ajoute les valeurs A et P à leurs listes
     SumA= sum(actif)
     SumP= sum(passif) #2 variables égales aux sommes des A et P
     print ("ACTIFS=", SumA)
@@ -38,5 +44,6 @@ def tab(n):
         print("oups,vous êtes en faillite..")
     elif SumA == SumP:
         print("les valeurs sont justes")
-        
+    
+    
     
